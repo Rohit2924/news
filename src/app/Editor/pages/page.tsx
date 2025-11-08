b847a76595f07b30b60b3721bfbdec2a60458075
Codedau <87289246+Codedau@users.noreply.github.com>
2025-11-08 09:05:30 +0545

    build

--- src/app/Editor/pages/page.tsx
+++ src/app/Editor/pages/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { PageProvider } from '@/context/PageContext';
 import PageManager from '@/components/admin/PageManager';
 import EditorLayoutWrapper from '@/components/editor/EditorLayoutWrapper';
